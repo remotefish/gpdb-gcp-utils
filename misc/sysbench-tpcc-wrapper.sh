50ed1474ef6cc404ba06304e108b15cb6ba0e562
Ning Yu <nyu@pivotal.io>
2020-02-17 09:44:38 +0800

    Add a missing wrapper file for sysbench-tpcc

---
+++ misc/sysbench-tpcc-wrapper.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+: ${T:=10}
+: ${N:=40}
+: ${SCALE:=10}
+: ${TABLES:=10}
+
+exec ./tpcc.lua \
+	--db-driver=pgsql \
+	--db-ps-mode=disable \
+	--db-debug=off \
+	--pgsql-host=localhost \
+	--pgsql-port=${PGPORT} \
+	--pgsql-db=tpcc \
+	--pgsql-user=$USER \
+	--time=$T \
+	--threads=$N \
+	--report-interval=1 \
+	--tables=$TABLES \
+	--scale=$SCALE \
+	"$@"
